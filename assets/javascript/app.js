19e112cc5e5acd00030c77a66c77ba5914071ae5
Alan Montoya <alanmontoya2010@att.net>
2019-04-05 22:18:38 -0500

    trying to create questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 // THIS IS ALL THE JQUERY AND JAVASCRIPT FOR MY TRIVIA GAME 
 // USED YOUTUBE VIDEOS AND A BLOG POST FOR INSPIRATION 
 
-
+// selecting HTML tags and storing variables like so
 const quizContainer=document.getElementById("quiz");
 
 const resultsContainer=document.getElementById("results");
@@ -19,10 +19,28 @@ function showResults (){
 
 }
 
-// this function will display quiz right away 
+// evoking this function will display quiz right away 
 
 buildQuiz ();
 
 // on submit it will show the results 
 
 submitButton.addEventListener('click'.showResults);
+
+
+
+//QUESTIONS 
+//USING AN ARRAY WILL MAKE THE QUERSTIONS EASY TO ITERATE OVER 
+
+const myQuestions = [
+    [
+        question: "Corey and Tapanga are from what show?";
+
+       var answers: {
+            a: "Full House",
+            b: "Beverley Hills 90210",
+            c: "Boy Meets World",
+        };
+        correctAnswer: "c";
+    ]
+]
