375acf68a10ccea8997687049b01c7f362c225a6
Alan Montoya <alanmontoya2010@att.net>
2019-04-05 22:06:02 -0500

    javascript skeleton created

---
+++ assets/javascript/app.js
@@ -0,0 +1,28 @@
+// THIS IS ALL THE JQUERY AND JAVASCRIPT FOR MY TRIVIA GAME 
+// USED YOUTUBE VIDEOS AND A BLOG POST FOR INSPIRATION 
+
+
+const quizContainer=document.getElementById("quiz");
+
+const resultsContainer=document.getElementById("results");
+
+const submitButton=document.getElementById("submit");
+
+
+//FUNCTIONS BEGIN BELOW 
+
+function buildQuiz () {
+
+}
+
+function showResults (){
+
+}
+
+// this function will display quiz right away 
+
+buildQuiz ();
+
+// on submit it will show the results 
+
+submitButton.addEventListener('click'.showResults);
