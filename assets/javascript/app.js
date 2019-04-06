f8f219b54f0301fed5203154b8de47105196900e
Alan Montoya <alanmontoya2010@att.net>
2019-04-05 22:49:56 -0500

    bootstrap added and questions in JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,14 +7,40 @@ $(document).ready(function () {
 //USING AN ARRAY WILL MAKE THE QUERSTIONS EASY TO ITERATE OVER 
 
 var myQuestions = [
-    {
         {
             question: "Corey and Tapanga are from what show?";
             choice:["Full House", "Beverley Hills 90210","Boy Meets World","Saved By The Bell"];
-            answer: 2,;
+            answer: 2;
             photo: "";
-         };
-    
+         },
+
+         {
+            question: "Which one of these spices were not part of The Spice Girls?";
+            choice:["Scary Spice", "Posh Spice","Tragic Spice","Baby Spice"];
+            answer: 2;
+            photo: "";
+         },
+         { 
+            question: "What movie coined the phrases 'AS IF' and 'Whatever!' ?";
+            choice:["It Takes Two", "Bebe's Kids","Encino Man","Clueless"];
+            answer: 3;
+            photo: "";
+         },
+         {
+            question: "What show were Tia Mowry and Tamera Mowry in?";
+            choice:["Sister, Sister", "All That","Full House","Dawson's Creek"];
+            answer: 0;
+            photo: "";
+         },
+         {
+            question: "Melissa Joan Hart played a teenager with powers in what show?";
+            choice:["Clarisa Explains It All", "Sabrina The Teenage Witch","So Weird","So Weird"];
+            answer: 1;
+            photo: "";
+         }
+        }]; // these are the closing tags for my array/objects of questions 
+
+         
 
 
 
