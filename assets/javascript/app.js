fb7870d865914b10ea8bd232f46c0098ae939eba
Alan Montoya <alanmontoya2010@att.net>
2019-04-05 22:34:50 -0500

    new javascript structure

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,46 +1,21 @@
 // THIS IS ALL THE JQUERY AND JAVASCRIPT FOR MY TRIVIA GAME 
 // USED YOUTUBE VIDEOS AND A BLOG POST FOR INSPIRATION 
-
-// selecting HTML tags and storing variables like so
-const quizContainer=document.getElementById("quiz");
-
-const resultsContainer=document.getElementById("results");
-
-const submitButton=document.getElementById("submit");
-
-
-//FUNCTIONS BEGIN BELOW 
-
-function buildQuiz () {
-
-}
-
-function showResults (){
-
-}
-
-// evoking this function will display quiz right away 
-
-buildQuiz ();
-
-// on submit it will show the results 
-
-submitButton.addEventListener('click'.showResults);
-
+$(document).ready(function () {
 
 
 //QUESTIONS 
 //USING AN ARRAY WILL MAKE THE QUERSTIONS EASY TO ITERATE OVER 
 
-const myQuestions = [
-    [
-        question: "Corey and Tapanga are from what show?";
+var myQuestions = [
+    {
+        {
+            question: "Corey and Tapanga are from what show?";
+            choice:["Full House", "Beverley Hills 90210","Boy Meets World","Saved By The Bell"];
+            answer: 2,;
+            photo: "";
+         };
+    
 
-       var answers: {
-            a: "Full House",
-            b: "Beverley Hills 90210",
-            c: "Boy Meets World",
-        };
-        correctAnswer: "c";
-    ]
-]
+
+
+} // THIS CURLY BRACE IS THE END OF MY JAVASCRIPT CODE 
